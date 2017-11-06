c1e9cf44555f7656271155ad08daf55e5158a165
bkaszuba <ziomal4444@gmail.com>
2017-11-06 19:27:41 +0100

    Created project for SISE project(solving fifteen puzzle)

---
+++ src/main/java/com/madbar/App.java
@@ -0,0 +1,13 @@
+package com.madbar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
